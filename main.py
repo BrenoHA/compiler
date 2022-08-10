3aa9db5fbcadf58c2a7777d3d1a3cf40be51de23
Breno Hasparyk <breno.ha@gmail.com>
2022-08-09 23:29:40 -0300

    Add: Parser to root

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-from lexical.symbol_table import *
-from lexical.scanner_mgol import *
 
 
 def main():
 
-    test_scanner = Scanner_mgol("test.txt")
-    test_scanner.scanner()
-    test_scanner.print_elements()
-    tokenArr = test_scanner.return_elements()
-    print(len(tokenArr))
-    print(tokenArr[1])
+    print(">>")
 
 
 main()
