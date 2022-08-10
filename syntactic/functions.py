1a37d7f86d50ace27f1d94a781a0b3dd90978613
Eduardo Silvestre <eduardospg2903@gmail.com>
2022-08-09 22:47:19 -0300

    Fix: replacing ; for , in the csv's Add: csv read and error filling

---
+++ syntactic/functions.py
@@ -0,0 +1,3 @@
+# def goto():
+
+# def action():
---
+++ syntactic/read_csv.py
@@ -0,0 +1,11 @@
+import csv
+import pandas as pd
+df_action = pd.read_csv('./public/action.csv')  
+df_goto = pd.read_csv('./public/goto.csv')  
+
+def fill_errors(df):
+    for index, state in enumerate(df['State']):
+        df[df['State']==index] = df[df['State']==index].fillna('erro'+str(state))
+    print(df)
+
+fill_errors(df_action)
