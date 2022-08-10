ff6e325f559a2c57a0a15b343a6306e5f86c8b63
Eduardo Silvestre <eduardospg2903@gmail.com>
2022-08-10 00:00:36 -0300

    Add: actions and goto functions Remove: read_csv.py

--- syntactic/functions.py
+++ syntactic/functions.py
@@ -1,3 +1,17 @@
-# def goto():
+import csv
+import pandas as pd
+df_action = pd.read_csv('./public/action.csv') 
+df_goto = pd.read_csv('./public/goto.csv')  
 
-# def action():
+def fill_errors(df):
+    for index, state in enumerate(df['State']):
+        df[df['State']==index] = df[df['State']==index].fillna('e'+str(state))
+
+def goto(estado,lexema):
+    fill_errors(df_goto)
+    return df_goto[lexema][estado]
+
+def action(lexema,estado):
+    fill_errors(df_action)
+    return df_action[lexema][estado]
+
